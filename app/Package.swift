4b244a5603d8444f4cd0cad9e9338958b393e1ce
Swizzlr <git@swizzlr.co>
2015-12-24 20:54:14 -0500

    It kind of works I guess sorta what.

--- app/Package.swift
+++ app/Package.swift
@@ -1,5 +1,7 @@
 import PackageDescription
 
 let package = Package(name: "ffmailman", dependencies: [
+  .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0),
+  .Package(url: "https://github.com/swizzlr/Underwood.git", majorVersion: 0),
   .Package(url: "https://github.com/kylef/Curassow.git", Version("0.2.0")!)
 ])
--- app/Sources/main.swift
+++ app/Sources/main.swift
@@ -1,6 +1,26 @@
 import Curassow
 import Inquiline
+import Underwood
+import Foundation
 
-serve { request in
-  return Response(.Ok, contentType: "text/plain", body: "Hello World")
+final class FuckFuckingMailman: 🇺🇸 {
+  override init() {
+    super.init()
+    get("/") { _ in
+      print("got request")
+      let query = "joe fucking groff eh"
+      let comps = NSURLComponents(string: "https://encrypted.google.com/search")!
+      let reply = NSURLQueryItem(name: "q", value: query)
+      let asSiteSearch = NSURLQueryItem(name: "as_sitesearch", value: "lists.swift.org")
+      comps.queryItems = [reply, asSiteSearch]
+      print(comps)
+      let redirectURL = comps.string!
+      print(redirectURL)
+      let r = Response(.Found, headers: [("Location", redirectURL)])
+      print(r)
+      return r
+    }
+  }
 }
+
+serve(FuckFuckingMailman().application)
