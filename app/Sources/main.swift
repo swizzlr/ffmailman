138e4cf8c1a450e48b65d12f1dc6549d61bbdd2e
Swizzlr <git@swizzlr.co>
2015-12-27 17:21:31 -0500

    Add dumb web index.

--- app/Sources/main.swift
+++ app/Sources/main.swift
@@ -6,17 +6,26 @@ import Foundation
 final class FuckFuckingMailman: 🇺🇸 {
   override init() {
     super.init()
-    get("/:query") { params in
+     get("/fuckit/searchit") { _, query in
+      let query = NSString(string: query["search"] ?? "You gotta give me a search term mate")
+      return redirectForQuery(query)
+    }
+
+    get("/:query") { params, _ in
       let query = NSString(string: params["query"]!)
-      let url = NSString(string: "https://encrypted.google.com/search?as_sitesearch=lists.swift.org&q=")
-      let escapedQuery = query
-        .stringByAddingPercentEncodingWithAllowedCharacters(
-          NSCharacterSet.URLQueryAllowedCharacterSet()
-        )!
-      let redirectURL = url.stringByAppendingString(escapedQuery)
-      return Response(.Found, headers: [("Location", redirectURL)])
+      return redirectForQuery(query)
     }
   }
+}
+
+private func redirectForQuery(query: NSString) -> Response {
+  let url = NSString(string: "https://encrypted.google.com/search?as_sitesearch=lists.swift.org&q=")
+  let escapedQuery = query
+    .stringByAddingPercentEncodingWithAllowedCharacters(
+      NSCharacterSet.URLQueryAllowedCharacterSet()
+    )!
+  let redirectURL = url.stringByAppendingString(escapedQuery)
+  return Response(.Found, headers: [("Location", redirectURL)])
 }
 
 serve(FuckFuckingMailman().application)
